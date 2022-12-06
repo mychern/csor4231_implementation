d013d4076f3b97544aea602a1582994d2cd39233
Markov Chen <mychen0912@gmail.com>
2022-12-06 10:39:14 -0500

    Modified unit test to cover char type.

--- src/sorting/merge_sort/merge_sort_test.cc
+++ src/sorting/merge_sort/merge_sort_test.cc
@@ -5,6 +5,7 @@ int main() {
     vector<int> test_1{1, 3, 5, 2, 4, 6};
     vector<int> test_2{4, 3, 2, 1};
     vector<int> test_3{-4, 1, -2, -5, 3, 2, 1, 5, 2};
+    vector<char> test_4{'a', 'z', 'c', 'j', 'b', 'd'};
 
     // Tests for merge(arr, p, q, r) function.
     cout<<"Merge function output test result:"<<endl;
@@ -15,6 +16,7 @@ int main() {
     mergeSort<int>(test_1, 0, test_1.size()-1);
     mergeSort<int>(test_2, 0, test_2.size()-1);
     mergeSort<int>(test_3, 0, test_3.size()-1);
+    mergeSort<char>(test_4, 0, test_4.size()-1);
 
     cout<<"First test result:"<<endl;
     printVector<int>(test_1);
@@ -22,6 +24,8 @@ int main() {
     printVector<int>(test_2);
     cout<<"Third test result:"<<endl;
     printVector<int>(test_3);
+    cout<<"Fourth test for another data type:"<<endl;
+    printVector<char>(test_4);
 
     return 1;
 }
